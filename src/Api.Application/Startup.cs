826691f65aafada27902d683a8248984cd3ff0e8
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2020-11-12 08:17:17 -0300

    Rodrigo Furlaneti

---
+++ src/Api.Application/Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using Api.CrossCutting.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+//using Microsoft.OpenApi.Models;
+
+namespace Application
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureService.ConfigureDependenciesService(services);
+            ConfigureRepository.ConfigureDependenciesRepository(services);
+            services.AddControllers();
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo
+            //     {
+            //         Version = "v1",
+            //         Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+            //         Description = "Arquitetura DDD",
+            //         TermsOfService = new Uri("http://www.mfrinfo.com.br"),
+            //         Contact = new OpenApiContact
+            //         {
+            //             Name = "Marcos Fabricio Rosa",
+            //             Email = "mfr@mail.com",
+            //             Url = new Uri("http://www.mfrinfo.com.br")
+            //         },
+            //         License = new OpenApiLicense
+            //         {
+            //             Name = "Termo de Licença de Uso",
+            //             Url = new Uri("http://www.mfrinfo.com.br")
+            //         }
+            //     });
+            // });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseSwagger();
+            // app.UseSwaggerUI(c =>
+            // {
+            //     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+            //     c.RoutePrefix = string.Empty;
+            // });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para Criar as Migrações
+            var connectionString = "Server=localhost;Port=3306;DataBase=world;Uid=root;Pwd=123456";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
